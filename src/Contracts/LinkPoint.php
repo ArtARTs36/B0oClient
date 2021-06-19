bba4649031d3d2876f7a0e30792d091a4978ab14
aukrainskiy <aukrainskiy@phoenixit.ru>
2021-06-19 23:53:30 +0300

    add return type

--- src/Contracts/LinkPoint.php
+++ src/Contracts/LinkPoint.php
@@ -2,7 +2,9 @@
 
 namespace ArtARTs36\B0oClient\Contracts;
 
+use ArtARTs36\B0oClient\Data\Link;
+
 interface LinkPoint
 {
-    public function cut(string $fullUrl);
+    public function cut(string $fullUrl): Link;
 }
